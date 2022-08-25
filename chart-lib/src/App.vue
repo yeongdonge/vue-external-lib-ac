6b280bb7fe5ff102f2c48ed8b263446e7902468d
yeongdonge <parrineau@gmail.com>
2022-08-25 20:23:50 +0900

    repo init

---
+++ chart-lib/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import {Chart, registerables}  from 'chart.js';
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
+Chart.register(...registerables);
+export default {
+  components: {
+    BarChart,
+    LineChart,
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<template>
+  <canvas id="myChart2"></canvas>
+</template>
+
+
+<script>
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+export default {
+  mounted() {
+    const labels = ["January", "February", "March", "April", "May", "June"];
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+
+    const config = {
+      type: "line",
+      data: data,
+      options: {},
+    };
+    const myChart2 = new Chart(document.getElementById("myChart2"), config);
+    console.log(myChart2);
+  },
+};
+</script>
+
+<style>
+</style>
